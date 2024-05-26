0c0ce5926a17f243399863bd635e2c9eba769fe0
Braian <braian.gonzales77@gmail.com>
2024-05-26 20:29:30 +0200

    test suite for new order controller

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -50,7 +50,7 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
 
-ticketSchema.statics.isReserved = async function () {
+ticketSchema.methods.isReserved = async function () {
   // Run query to look at all orders. Find an order where the ticket
   // is the ticket we just found *and* the orders status is *not* cancelled.
   // If we find an order from that means the ticket *is* reserved
