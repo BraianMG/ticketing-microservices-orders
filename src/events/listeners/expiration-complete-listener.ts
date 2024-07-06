016b1480964bfc7587fd7df312e7a95dea5c2464
Braian <braian.gonzales77@gmail.com>
2024-07-06 21:31:00 +0200

    emmit the order cancelled event

--- src/events/listeners/expiration-complete-listener.ts
+++ src/events/listeners/expiration-complete-listener.ts
@@ -9,6 +9,7 @@ import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
 import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
@@ -18,7 +19,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     data: ExpirationCompleteEvent['data'],
     msg: Message,
   ): Promise<void> {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate('ticket');
 
     if (!order) {
       throw new Error('Order not found');
@@ -26,7 +27,18 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
     order.set({
       status: OrderStatus.Cancelled,
-      ticket: null,
     });
+
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
   }
 }
