9385dd4372c63a03dda0d99dee543b4391f94a78
Braian <braian.gonzales77@gmail.com>
2024-07-06 21:25:02 +0200

    handle an expiration event

---
+++ src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,32 @@
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+  TicketCreatedEvent,
+} from '@braianmg-ticketing/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { Order } from '../../models/order';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(
+    data: ExpirationCompleteEvent['data'],
+    msg: Message,
+  ): Promise<void> {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+      ticket: null,
+    });
+  }
+}
